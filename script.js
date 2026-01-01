4f9fda7d0f633950233f8edcba6c4aedeb73f70f
priyanshmittal13-debug <priyanshmittal13@gmail.com>
2026-01-02 01:01:51 +0530

    script.js

--- script.js
+++ script.js
@@ -3,13 +3,32 @@ $(document).ready(function(){
     let boxMail = $('.boxMail');
     let close = $('.fa-xmark');
 
-    // Open letter box on button click
-    mailBox.click(function(){
-        boxMail.addClass('active');
-    });
+    mailBox.click(function(){ boxMail.addClass('active'); });
+    close.click(function(){ boxMail.removeClass('active'); });
 
-    // Close letter box
-    close.click(function(){
-        boxMail.removeClass('active');
-    });
+    // Confetti creation
+    const confettiContainer = document.getElementById('confetti');
+
+    function createConfettiPiece() {
+        const confetti = document.createElement('div');
+        confetti.classList.add('confetti');
+
+        // Random colors
+        const colors = ['#ff4500','#ffdd00','#00bfff','#ff69b4','#32cd32'];
+        confetti.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+
+        // Random position
+        confetti.style.left = Math.random() * window.innerWidth + 'px';
+        confetti.style.width = 5 + Math.random()*10 + 'px';
+        confetti.style.height = 5 + Math.random()*10 + 'px';
+        confetti.style.animationDuration = 3 + Math.random()*5 + 's';
+
+        confettiContainer.appendChild(confetti);
+
+        // Remove after animation
+        setTimeout(()=> { confetti.remove(); }, 7000);
+    }
+
+    // Generate confetti continuously
+    setInterval(createConfettiPiece, 150);
 });
